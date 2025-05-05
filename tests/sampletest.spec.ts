4103e8eaca5174f701d144f7f1f344042999b42b
HarshAg447 <harshagarwalwork731@gmail.com>
2025-05-05 15:06:33 +0530

    Implemented testControl logic

--- tests/sampletest.spec.ts
+++ tests/sampletest.spec.ts
@@ -1,20 +1,30 @@
 import test, { expect } from "@playwright/test";
 import { LoginPage } from "../pages/Login";
 import { Logger } from "../utils/Logger";
+import { TestRunner } from "../utils/TestControl";
 
-let logger: Logger;
 
-test("Login test", async ({ page }, testinfo) => {
+test.describe("Login test", ()=> {
+    let logger: Logger;
+    let testRunner:TestRunner = new TestRunner();;
+
+testRunner.shouldRun("Positive Login test") &&
+test(`${testRunner.getTestCaseName()}`, async ({ page }, testinfo,) => {
+    testinfo.retry = testRunner.getRetries()?? 0;
+    
     logger = new Logger(testinfo.title)
     const login = new LoginPage(page, logger);
     await page.goto("/login");
-    await login.login("tomsmith", "SuperSecretPassword!");
+    await login.login(testRunner.getTestData("username"), testRunner.getTestData("password"));
  
     const message = await login.getFlashMessage();
     await login.checkForMessage("You logged into a secure area!");
 });
 
-test("Invalid login should show error message", async ({ page }, testinfo) => {
+testRunner.shouldRun("Negative Login test") &&
+test(`${testRunner.getTestCaseName()}`, async ({ page }, testinfo) => {
+    testinfo.retry = testRunner.getRetries()?? 0;
+    
     logger = new Logger(testinfo.title)
     const login = new LoginPage(page, logger);
 
@@ -27,4 +37,6 @@ test("Invalid login should show error message", async ({ page }, testinfo) => {
 
 test.afterEach(() => {
     logger.close();
+})
+
 })
---
+++ utils/TestControl.ts
@@ -0,0 +1,47 @@
+
+import * as fs from 'fs';
+
+
+import * as fs from 'fs';
+
+interface TestRun {
+    testCaseName: string;
+    run: boolean;
+    retries?: number;
+    data: { [key: string]: string };  
+}
+
+
+
+export class TestRunner {
+    data:any;    
+    position:number= 0;
+    constructor(){
+        const file = fs.readFileSync("C:\\Nitish\\Playwright Framework\\datasources\\TestControl.json", "utf-8");
+        this.data = JSON.parse(file);
+    }
+
+    public shouldRun(testCaseName:string):boolean{
+        for (let index = 0; index < this.data.length; index++) {
+            const element = this.data[index];
+            if(element.testCaseName == testCaseName){
+                this.position=index;
+                return element.run;
+            }
+        }
+        return false;
+    }
+
+    public getTestCaseName():any{
+        return this.data[this.position]["testCaseName"];
+    }
+
+    public getRetries():any{
+        return this.data[this.position]["retries"];
+    }
+
+    public getTestData(data:string):string{
+        let Storage= this.data[this.position]["data"];
+        return Storage[data] ?? "";
+    }
+}
